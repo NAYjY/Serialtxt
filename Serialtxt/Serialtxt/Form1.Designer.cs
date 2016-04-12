90a20d51db24b48aeca6566c538f07b636dbe83e
NAYjY <NCKCVCK@gmail.com>
2016-04-12 15:26:14 +0700

    Added files via upload

---
+++ Serialtxt/Serialtxt/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Serialtxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.PATH = new System.Windows.Forms.TextBox();
+            this.OPEN = new System.Windows.Forms.Button();
+            this.CLOSE = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.COM = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(14, 38);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(484, 313);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // PATH
+            // 
+            this.PATH.Location = new System.Drawing.Point(14, 12);
+            this.PATH.Name = "PATH";
+            this.PATH.Size = new System.Drawing.Size(246, 20);
+            this.PATH.TabIndex = 2;
+            this.PATH.Text = "C:\\New folder\\";
+            // 
+            // OPEN
+            // 
+            this.OPEN.Location = new System.Drawing.Point(377, 13);
+            this.OPEN.Name = "OPEN";
+            this.OPEN.Size = new System.Drawing.Size(56, 19);
+            this.OPEN.TabIndex = 3;
+            this.OPEN.Text = "OPEN";
+            this.OPEN.UseVisualStyleBackColor = true;
+            this.OPEN.Click += new System.EventHandler(this.OPEN_Click);
+            // 
+            // CLOSE
+            // 
+            this.CLOSE.Location = new System.Drawing.Point(439, 12);
+            this.CLOSE.Name = "CLOSE";
+            this.CLOSE.Size = new System.Drawing.Size(59, 19);
+            this.CLOSE.TabIndex = 4;
+            this.CLOSE.Text = "CLOSE";
+            this.CLOSE.UseVisualStyleBackColor = true;
+            this.CLOSE.Click += new System.EventHandler(this.CLOSE_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 57600;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // COM
+            // 
+            this.COM.FormattingEnabled = true;
+            this.COM.Location = new System.Drawing.Point(275, 10);
+            this.COM.Name = "COM";
+            this.COM.Size = new System.Drawing.Size(84, 21);
+            this.COM.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 368);
+            this.Controls.Add(this.COM);
+            this.Controls.Add(this.CLOSE);
+            this.Controls.Add(this.OPEN);
+            this.Controls.Add(this.PATH);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Serial txt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox PATH;
+        private System.Windows.Forms.Button OPEN;
+        private System.Windows.Forms.Button CLOSE;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox COM;
+    }
+}
+
---
+++ Serialtxt/Serialtxt/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace Serialtxt
+{
+    public partial class Form1 : Form
+    {
+        string line;
+
+        ///Stop Watch
+        ///
+        Stopwatch stopWatch = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] strPortNames = SerialPort.GetPortNames();
+            foreach (string n in strPortNames)
+            {
+                COM.Items.Add(n);
+            }
+             COM.SelectedIndex = 0;
+       
+            
+        }
+
+        private void OPEN_Click(object sender, EventArgs e)
+        {
+            
+            serialPort1.PortName = COM.SelectedItem + "";
+            serialPort1.Open();
+            stopWatch.Start(); // Stop watch
+            if (serialPort1.IsOpen)
+            {
+                OPEN.Enabled = false;
+                CLOSE.Enabled = true;
+                textBox.ReadOnly = false;
+            }
+
+        }
+
+        int i = 0;
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            line = serialPort1.ReadLine; /// txt
+            textBox.AppendText(line);
+            textBox.AppendText("\n");
+            Console.WriteLine(i++);
+
+            //txt///
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@PATH.Text+".txt", true))
+            {
+                file.WriteLine(line + '\t'+ 
+                stopWatch.Elapsed.Milliseconds.ToString());
+            }
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void CLOSE_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                OPEN.Enabled = true;
+                CLOSE.Enabled = false;
+                textBox.ReadOnly = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+        }
+    }
+}
